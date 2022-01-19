2c2a84ec2f7c489768d1ee698b314b26d3514506
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-19 12:45:30 +0000

    add some unit tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
 const supertest = require("supertest");
 const app = require("../server");
+
 describe("Array", function () {
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
@@ -22,15 +23,50 @@ describe("GET /", function () {
 });
 
 /*describe("POST /", function () {
-  it("it should has status code 200", function (done) {
+  it("it should send json payload", function (done) {
     supertest(app)
       .post("/")
       .send({ id: 1 })
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .end(function (err, res) {
-        if (err) done(err);
-        done();
+      .then((response) => {
+        // Check the response data
+        expect(response.body).toBe({ id: 1 });
       });
   });
 });*/
+
+/*describe("Some module", function () {
+  //group uni tests
+  it("does some thing", function () {
+    assert(someModule.doesSomeThing()); //unit test
+  });
+});*/
+
+describe("My feature", function () {
+  before("optional description", function () {
+    console.log("before test run");
+  });
+
+  after("optional description", function () {
+    console.log("after test run");
+  });
+
+  beforeEach("optional description", function () {
+    console.log("beforeEach test run");
+  });
+
+  afterEach("optional description", function () {
+    console.log("afterEach test run");
+  });
+
+  describe("subfeature 1", function () {
+    it("does one thing", function () {});
+
+    it("does another thing", function () {});
+  });
+
+  describe("subfeature 2", function () {
+    it("does one thing", function () {});
+
+    it("does another thing", function () {});
+  });
+});
